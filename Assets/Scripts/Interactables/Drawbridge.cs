dc86d4bcf247df566a16a965747bf65162c121af
Dan Cortens <dan_cortens@hotmail.com>
2022-11-22 16:53:51 -0500

    made drawbridge pretty

--- Assets/Scripts/Interactables/Drawbridge.cs
+++ Assets/Scripts/Interactables/Drawbridge.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Drawbridge : MonoBehaviour
 {
     private bool closed;
-    private AudioSource bridgeSounds;
-    public AudioClip openSound;
+    public string soundName;
     public GameObject bridge;
+    public LineRenderer line0;
+    public LineRenderer line1;
+    public Transform[] points;
     public float openPos;
 
     private void Start()
     {
-        openPos = -1f;
 
         if (PlayerPrefs.HasKey(gameObject.name))
         {
@@ -25,21 +26,40 @@ public class Drawbridge : MonoBehaviour
         }
         if (!closed)
             bridge.transform.localPosition = new Vector3(bridge.transform.localPosition.x, openPos);
+        line0.SetPosition(0, points[0].position);
+        line0.SetPosition(1, points[1].position);
+        line1.SetPosition(0, points[2].position);
+        line1.SetPosition(1, points[3].position);
     }
 
+    public bool IsClosed()
+    {
+        return closed;
+    }
     public void Open()
     {
         closed = false;
-        PlayerPrefs.SetInt(gameObject.name, 0);
-        bridgeSounds.PlayOneShot(openSound);
-        InvokeRepeating("Opening", 0f, 0.05f);
+        FindObjectOfType<AudioManager>().Play(soundName);
+        InvokeRepeating("Opening", 0f, 0.05f); 
     }
 
     private void Opening()
     {
         if (bridge.transform.localPosition.y > openPos)
+        {
             bridge.transform.localPosition -= new Vector3(0f, 0.05f, 0f);
+            line0.SetPosition(0, points[0].position);
+            line0.SetPosition(1, points[1].position);
+            line1.SetPosition(0, points[2].position);
+            line1.SetPosition(1, points[3].position);
+        }
+            
         else
             CancelInvoke("Opening");
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt(gameObject.name, (closed) ? 0 : 1);
     }
 }
--- Assets/Scripts/Interactables/DrawbridgeSwitch.cs
+++ Assets/Scripts/Interactables/DrawbridgeSwitch.cs
@@ -6,9 +6,26 @@ public class DrawbridgeSwitch : Interactable
 {
     //switch for permanently unlocking a door
     public GameObject connectedDoor;
+    private Animator anim;
+    public LineRenderer line;
+    public Transform endpoint;
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        line.SetPosition(1, transform.position);
+        line.SetPosition(0, endpoint.position);
+    }
 
     public override void Interact()
     {
-        connectedDoor.GetComponent<Drawbridge>().Open();
+        if (connectedDoor.GetComponent<Drawbridge>().IsClosed())
+        {
+            anim.SetTrigger("interact");
+            connectedDoor.GetComponent<Drawbridge>().Open();
+        }
+            
     }
 }
