b9c48647d4a53e97a81acede329d8c50b087bd98
Dan Cortens <dan_cortens@hotmail.com>
2022-11-22 20:58:39 -0500

    made barrier puzzle pretty

--- Assets/Scripts/Interactables/Barrier.cs
+++ Assets/Scripts/Interactables/Barrier.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Barrier : MonoBehaviour
 {
     public int id;
-    public Vector3 closedPos;
+    public Vector3 startPos;
     public Vector3 openPos;
     public float toggleTime;
     public bool open;
+    public LineRenderer line;
+    public Vector3 lineOffset;
     private bool switching;
     private float slide;
     // Start is called before the first frame update
@@ -16,10 +18,14 @@ public class Barrier : MonoBehaviour
     {
         if (PlayerPrefs.HasKey(gameObject.name + id))
             open = (PlayerPrefs.GetInt(gameObject.name + id) == 0) ? false : true;
-        
-        transform.position = (open) ? openPos : closedPos;
+        startPos = transform.position;
+        if (open)
+            transform.position += openPos;
         switching = false;
         slide = 0f;
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, transform.position + openPos + lineOffset);
+        
     }
 
     // Update is called once per frame
@@ -31,17 +37,19 @@ public class Barrier : MonoBehaviour
             if (slide < toggleTime)
             {
                 if (open)
-                    transform.position = Vector3.Lerp(openPos, closedPos, slide / toggleTime);
+                    transform.position = Vector3.Lerp(startPos + openPos, startPos, slide / toggleTime);
                 else
-                    transform.position = Vector3.Lerp(closedPos, openPos, slide / toggleTime);
+                    transform.position = Vector3.Lerp(startPos, startPos + openPos, slide / toggleTime);
             }
             else
             {
                 open = !open;
                 switching = false;
-                transform.position = (open) ? openPos : closedPos;
+                transform.position = startPos;
+                if (open)
+                    transform.position += openPos;
             }
-            
+            line.SetPosition(0, transform.position);
         }
     }
 
--- Assets/Scripts/Interactables/BarrierLever.cs
+++ Assets/Scripts/Interactables/BarrierLever.cs
@@ -7,9 +7,18 @@ public class BarrierLever : Interactable
     public Barrier[] connectedBarriers;
     public float toggleTime;
     [SerializeField] private bool toggling;
+    private Animator anim;
+    public LineRenderer line;
+    public Transform endpoint;
     private void Start()
     {
         toggling = false;
+        anim = gameObject.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        line.SetPosition(1, transform.position);
+        line.SetPosition(0, endpoint.position);
     }
     public override void Interact()
     {
@@ -18,6 +27,7 @@ public class BarrierLever : Interactable
             toggling = true;
             foreach (Barrier b in connectedBarriers)
                 b.Toggle();
+            anim.SetTrigger("interact");
             StartCoroutine(ToggleTime());
         }
             
